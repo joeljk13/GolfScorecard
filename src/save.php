aec87ef742d3f46405162bddb5b69852ed6bcee5
Joel Kottas <sumnon@cj3.org>
2015-07-11 00:27:19 -0400

    Update save.php
     - Use scorecard id as file name to save scores to
     - Don't every send any text back from server
     - Instead, send appropriate HTTP error codes
     - Also, error check the fwrite call

--- src/save.php
+++ src/save.php
@@ -1,15 +1,41 @@
 <?php
 
+function is_valid_scorecard_id($id) {
+    if ($id === "" || preg_match('/\W/', $id) === 1) {
+        return false;
+    }
+    return true;
+}
+
+function error($code) {
+    if ($code === 400) {
+        header('HTTP/1.1 400 Bad Request');
+    } else {
+        header('HTTP/1.1 500 Internal Server Error');
+    }
+    exit();
+}
+
 if (!isset($_POST['json'])) {
-    die("Error 0");
+    error(400);
 }
 
 $json = json_decode($_POST['json'], true);
 if ($json === NULL) {
-    die("Error 1");
+    error(400);
+}
+
+$id = $json['id'];
+if (!is_valid_scorecard_id($id)) {
+    error(400);
 }
 
 $json = json_encode($json, JSON_PRETTY_PRINT);
-$file = fopen("scores.txt", "w") or die("Error 2");
-fwrite($file, "$json\n");
+
+$file = fopen("scores/$id", "w") or error(500);
+$write = fwrite($file, "$json\n");
 fclose($file);
+
+if (!$write) {
+    error(500);
+}
